9593852d9471fa5a790d8ede8061a408995f2114
chikealvan <nwadoziechikealvan@gmail.com>
2023-10-05 11:52:52 +0100

    first commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# list of approved dog breeds
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +13,34 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    # Initialize the Dog class with a name and breed
+    def __init__(self, name, breed):
+        self._name = name
+        self._breed = breed
+
+    # Define a property for the 'name' attribute
+    @property
+    def name(self):
+        return self._name
+
+    # Setter method for 'name' attribute with validation
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    # Define a property for the 'breed' attribute
+    @property
+    def breed(self):
+        return self._breed
+
+    # Setter method for 'breed' attribute with validation using APPROVED_BREEDS
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in the list of approved breeds.")
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,42 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    # Initialize the Person class with a name and job
+    def __init__(self, name, job):
+        self._name = self._validate_name(name)
+        self._job = self._validate_job(job)
+
+    # Define a property for the "name" attribute
+    @property
+    def name(self):
+        return self._name
+
+    # Setter method for "name" attribute with validation
+    @name.setter
+    def name(self, value):
+        self._name = self._validate_name(value)
+
+    # Define a property for the "job" attribute
+    @property
+    def job(self):
+        return self._job
+
+    # Setter method for "job" attribute with validation using APPROVED_JOBS
+    @job.setter
+    def job(self, value):
+        self._job = self._validate_job(value)
+
+    # Helper method to validate and format the name
+    def _validate_name(self, value):
+        if isinstance(value, str) and len(value) < 25:
+            return value.title()
+        else:
+            print("Name must be a string under 25 characters.")
+            return ""
+
+    # Helper method to validate the job using APPROVED_JOBS
+    def _validate_job(self, value):
+        if value in APPROVED_JOBS:
+            return value
+        else:
+            print("Job must be in the list of approved jobs.")
